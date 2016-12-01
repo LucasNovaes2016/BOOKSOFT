6b66cdca96b9450a9b935198209507bd3cd8e2d2
Blumenkranz <Blumenkranz@users.noreply.github.com>
2016-11-30 23:54:59 -0300

    Delete cliente.cpp

--- cliente.cpp
+++
@@ -1,7 +0,0 @@
-#include "cliente.hpp"
-
-Cliente::Cliente(Pessoa_fisica pf, Pessoa_juridica pj/*, exclusividades de clientes*/) 
-	: Pessoa_fisica{pf}, Pessoa_fisica{pj}/*, exclusividades de clientes*/ {}
-
-Cliente::Cliente()
-	: Pessoa_fisica{}, Pessoa_juridica {}/*, exclusividades de clientes*/ {}
